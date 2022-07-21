60b7eaee92d349637589c1909d8cbe12ef632e4a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-07-21 19:45:32 +0000

    chore: add the linter for react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
 , parser: '@typescript-eslint/parser'
 , plugins: [
     '@typescript-eslint'
+  , 'react-hooks'
   ]
 , extends: [
     'eslint:recommended'
   , 'plugin:@typescript-eslint/recommended'
+  , 'plugin:react-hooks/recommended'
   ]
 , rules: {
     '@typescript-eslint/ban-types': 'off'
