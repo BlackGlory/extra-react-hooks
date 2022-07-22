186b9ea92f46a5449baf9de0b4af8405f3b4598b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-07-22 08:58:24 +0000

    test: add cases for useState

---
+++ __tests__/use-state.spec.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { renderHook } from '@testing-library/react-hooks/dom'
+
+describe('useState', () => {
+  it('does not return the same array', () => {
+    const { result, rerender } = renderHook(() => useState({}))
+
+    const arr1 = result.current
+    rerender()
+    const arr2 = result.current
+
+    expect(arr2).not.toBe(arr1)
+  })
+
+  it('returns the same state and setter', () => {
+    const { result, rerender } = renderHook(() => useState({}))
+
+    const [state1, setState1] = result.current
+    rerender()
+    const [state2, setState2] = result.current
+
+    expect(state2).toBe(state1)
+    expect(setState2).toBe(setState1)
+  })
+})
